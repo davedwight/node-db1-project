f9d632d943d0398c3ba55dbb06abdff6836335a2
davedwight <dpdwight@gmail.com>
2021-07-12 22:55:30 -0600

    checkAccountId and checkAccountNameUnique finished

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,10 +1,14 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const Account = require('./accounts-model');
+const Account = require("./accounts-model");
 
-const { checkAccountPayload } = require('./accounts-middleware');
+const {
+  checkAccountPayload,
+  checkAccountId,
+  checkAccountNameUnique,
+} = require("./accounts-middleware");
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     const data = await Account.getAll();
     res.json(data);
@@ -12,53 +16,52 @@ router.get('/', async (req, res, next) => {
     console.log(err);
     next(err);
   }
-})
+});
 
-router.get('/:id', async (req, res, next) => {
-  try {
-    const data = await Account.getById(req.params.id);
-    res.json(data);
-  } catch (err) {
-    console.log(err);
-    next(err);
-  }
-})
+router.get("/:id", checkAccountId, async (req, res, next) => {
+  res.json(req.account);
+});
 
-router.post('/', checkAccountPayload, async (req, res, next) => {
+router.post("/", checkAccountPayload, checkAccountNameUnique, async (req, res, next) => {
   try {
     const post = await Account.create(req.body);
     res.json(post);
   } catch (err) {
     next(err);
   }
-})
+});
 
-router.put('/:id', checkAccountPayload, async (req, res, next) => {
+router.put(
+  "/:id",
+  checkAccountId,
+  checkAccountPayload,
+  checkAccountNameUnique,
+  async (req, res, next) => {
+    try {
+      const updatedRecord = await Account.updateById(req.params.id, req.body);
+      res.json(updatedRecord);
+    } catch (err) {
+      console.log(err);
+      next(err);
+    }
+  }
+);
+
+router.delete("/:id", checkAccountId, async (req, res, next) => {
   try {
-    const updatedRecord = await Account.updateById(req.params.id, req.body);
-    console.log(updatedRecord);
-    res.json(updatedRecord);
+    await Account.deleteById(req.params.id);
+    res.json(req.account);
   } catch (err) {
-    console.log(err);
     next(err);
   }
 });
 
-router.delete('/:id', async (req, res, next) => {
-  try {
-    const deletedRecord = await Account.deleteById(req.params.id);
-    console.log(deletedRecord);
-    res.json(deletedRecord);
-  } catch (err) {
-    next(err);
-  }
-})
-
-router.use((err, req, res, next) => { // eslint-disable-line
+router.use((err, req, res, next) => {
+  // eslint-disable-line
   res.status(err.status || 500).json({
     message: err.message,
     stack: err.stack,
-  })
-})
+  });
+});
 
 module.exports = router;
