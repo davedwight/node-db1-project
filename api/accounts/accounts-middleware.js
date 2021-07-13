19a2f41416e6b973ae9f7019e44fbcc7ab70b134
davedwight <dpdwight@gmail.com>
2021-07-12 22:12:54 -0600

    string validation on req.body.name working

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -4,6 +4,11 @@ exports.checkAccountPayload = (req, res, next) => {
       status: 400,
       message: "name and budget are required",
     })
+  } else if (typeof req.body.name !== 'string') {
+      next({
+        status: 400,
+        message: "name of account must be a string",
+      })
   } else {
     next();
   }
